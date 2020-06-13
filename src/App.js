3af5eb9426c85e7bb5a021b031b15f2abb0a9c7a
ahamideh <ahamideh18@hotmail.com>
2020-06-13 14:46:01 -0400

    Added navbar to website

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ import {
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch> 
       {/* Switch only matches the firls url it finds */}
         <Route exact path='/' component={HomePage}/>
