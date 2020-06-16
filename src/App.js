3991453993ec1425ecce99029a5fe914c7a2d36f
ahamideh <ahamideh18@hotmail.com>
2020-06-16 12:01:13 -0400

    Added cart icon, cart reducer, and cart dropdown functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {
   Switch,
   Route,
+  Redirect
 } from "react-router-dom";
 import { connect } from 'react-redux';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
-import signInandSignUpPage from './pages/signInandSignUp/sign-in.component'
+import SignInandSignUpPage from './pages/signInandSignUp/sign-in.component'
 import Header from './components/header/header.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions';
@@ -47,15 +48,21 @@ class App extends React.Component {
           {/* Switch only matches the first url it finds */}
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signIn' component={signInandSignUpPage} />
+          <Route exact path='/signIn' render={() =>
+            this.props.currentUser ?
+              (<Redirect to='/' />) : (<SignInandSignUpPage />)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
