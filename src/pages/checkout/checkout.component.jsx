bb2f812aa9b18b5f03e52e1a6276b07937e6512b
ahamideh <ahamideh18@hotmail.com>
2020-06-18 00:04:25 -0400

    Added Stripe integration, changed favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import {
     selectCartItems,
@@ -34,11 +35,12 @@ const CheckoutPage = ({ cartItems, total }) => (
             <CheckoutItem key={cartItem.id} cartItem={cartItem} />
         ))}
         <div className='total'>TOTAL: ${total}</div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-    cartItems: selectCartItems,
+            cartItems: selectCartItems,
     total: selectCartTotal
 });
 
