b201e90d4ee363b0ce07e2b113a09119d3c8fb95
ahamideh <ahamideh18@hotmail.com>
2020-06-19 23:23:24 -0400

    Migrated all shop data to Firestore and added link feature in collection preview page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
   Redirect
 } from "react-router-dom";
 import { connect } from 'react-redux';
-import {createStructuredSelector} from 'reselect';
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
@@ -16,17 +16,19 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component'
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument, /* addCollectionAndDocuments */ } from './firebase/firebase.utils'
 
 import { setCurrentUser } from './redux/user/user.actions';
-import { selectCurrentUser } from './redux/user/user.selector'
+import { selectCurrentUser } from './redux/user/user.selector';
+/* import { selectCollectionsForPreview } from './redux/shop/shop.selectors'; */
+
 
 class App extends React.Component {
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser/*, collectionsArray*/ } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -40,6 +42,7 @@ class App extends React.Component {
         });
       }
       setCurrentUser(userAuth);
+      /*addCollectionAndDocuments('collections', collectionsArray.map(({ title, items }) => ({ title, items })));*/
     });
   }
 
@@ -66,7 +69,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  /*collectionsArray: selectCollectionsForPreview*/
 })
 
 const mapDispatchToProps = dispatch => ({
