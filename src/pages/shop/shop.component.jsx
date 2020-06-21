12ed724ab74c8f7d6c2292bdc7dddcc76a93123a
ahamideh <ahamideh18@hotmail.com>
2020-06-21 12:03:38 -0400

    Used redux-thunk to handle asynchornous redux events

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.components';
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+// import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
-import { updateCollections } from '../../redux/shop/shop.actions'
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions'
+import { selectIsCollectionFetching, selectIsCollectionLoaded } from '../../redux/shop/shop.selectors';
 
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
@@ -15,39 +17,51 @@ const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
-    state = {
-        loading: true
-    }
-    unsubscribeFromSnapshot = null;
-
     componentDidMount() {
-        const { updateCollections } = this.props;
-        const collectionRef = firestore.collection('collections');
-
-        // fetch('https://firestore.googleapis.com/v1/projects/glo-reactapp/databases/(default)/documents/collections')
-        // .then(res => res.json())
-        // .then(collections => console.log(collections))
-        this.unsubscribeFromSnapshot = collectionRef.get().then((snapshot => {
-            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-            updateCollections(collectionsMap);
-            this.setState({ loading: false });
-        }))
+        const { fetchCollectionsStartAsync } = this.props;
+        fetchCollectionsStartAsync();
     }
+
+    // state = {
+    //     loading: true
+    // }
+    // unsubscribeFromSnapshot = null;
+
+    // componentDidMount() {
+    //     const { updateCollections } = this.props;
+    //     const collectionRef = firestore.collection('collections');
+
+    //     // fetch('https://firestore.googleapis.com/v1/projects/glo-reactapp/databases/(default)/documents/collections')
+    //     // .then(res => res.json())
+    //     // .then(collections => console.log(collections))
+    //     this.unsubscribeFromSnapshot = collectionRef.get().then((snapshot => {
+    //         const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //         updateCollections(collectionsMap);
+    //         this.setState({ loading: false });
+    //     }))
+    // }
 
     render() {
-        const { match } = this.props;
-        const { loading } = this.state;
+        const { match, isCollectionFetching, isCollectionLoaded } = this.props;
+
         return (
             <div className="shop-page">
-                <Route exact path={`${match.path}`} render={(props) => < CollectionsOverviewWithSpinner isLoading={loading} {...props} />} />
-                <Route path={`${match.path}/:collectionId`} render={(props) => < CollectionPageWithSpinner isLoading={loading} {...props} />} />
+                <Route exact path={`${match.path}`} render={(props) => < CollectionsOverviewWithSpinner isLoading={isCollectionFetching} {...props} />} />
+                <Route path={`${match.path}/:collectionId`} render={(props) => < CollectionPageWithSpinner isLoading={!isCollectionLoaded} {...props} />} />
             </div>
         )
     }
 }
 
-const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionsMap => (dispatch(updateCollections(collectionsMap)))
+const mapStateToProps = createStructuredSelector({
+    isCollectionFetching: selectIsCollectionFetching,
+    isCollectionLoaded: selectIsCollectionLoaded
 })
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+const mapDispatchToProps = dispatch => ({
+    // updateCollections: collectionsMap => (dispatch(updateCollections(collectionsMap)))
+
+    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
