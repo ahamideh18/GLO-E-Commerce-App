ebf24c6247cdbcbe2e7271453612789b6b5b533a
ahamideh <ahamideh18@hotmail.com>
2020-08-29 19:26:54 -0400

    Stripe-button-component error

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -5,24 +5,19 @@ import trophyFillComp from '../../assets/trophyFillComp.svg'
 import { API } from 'aws-amplify';
 import axios from 'axios';
 
-const postData = {
-    amount: priceForStripe,
-    token
-}
-
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51GvDkNBDHRwhBcsS9Pc0lCayoiCAMZd5LubvFeXlFd6sjGRK6nEVxDOqmd5u5VM2PSRpsPqi9yOIDfkqJjPA7dPl00avHpaF5Q';
 
     const onToken = token => {
-        API.post('stripePayment', '/payment', postData)
-        .then(response => {
-            alert('Payment Successful')
-        }).catch(
-            error => {
-                console.log('Payment Error', JSON.parse(error));
-                alert('Issue with your payment')
-            })
+        API.post('stripePayment', '/payment', { amount: priceForStripe, token })
+            .then(response => {
+                alert('Payment Successful')
+            }).catch(
+                error => {
+                    console.log('Payment Error', JSON.parse(error));
+                    alert('Issue with your payment')
+                })
     }
 
     return (
